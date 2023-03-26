917c1f588d3a3fa37be4580cf8432622c32b9db2
FranciscoSalanova <fsalanova.16@gmail.com>
2023-03-26 20:12:32 -0300

    adding video section

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,10 @@ import images from './images'
 import data from './data'
 import meal from '../assets/meal.mp4'
 
-export default { images, data, meal }
+const index = {
+  images,
+  data,
+  meal
+}
+
+export default index
--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import './Intro.css'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import index from '../../constants/index'
 
 const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false)
+  const vidRef = React.useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if (playVideo) {
+      vidRef.current.pause()
+    } else {
+      vidRef.current.play()
+    }
+  }
+
   return (
-    <div>Intro</div>
+    <div className='app__video'>
+      <video
+        src={index.meal}
+        type='video/mp4'
+        ref={vidRef}
+        loop
+        controls={false}
+        muted
+      />
+      <div className='app__video-overlay flex__center'>
+        <div className='app__video-overlay_circle flex__center' onClick={handleVideo}>
+          {
+            (playVideo)
+              ? <BsFillPlayFill color='#FFF' fontSize={30} />
+              : <BsPauseFill color='#FFF' fontSize={30} />
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
